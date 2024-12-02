f8f33ae2c8d82e264c80b2ecb434baddcf4603cd
Precious <>
2024-12-02 21:00:04 +0100

    Subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,20 @@
+USE Airbnb;
+
+-- A query to find all properties where the average rating is greater than 4.0 using a subquery.
+SELECT name
+FROM Property
+WHERE id IN (
+  SELECT property_id
+  FROM Review
+  WHERE AVG(rating) > 4.0
+);
+
+-- A correlated subquery to find users who have made more than 3 bookings.
+SELECT
+  CONCAT(first_name, ' ', last_name) AS Person
+FROM User
+WHERE (
+  SELECT COUNT(*)
+  FROM Booking
+  WHERE Booking.user_id = User.id
+  ) > 3;
